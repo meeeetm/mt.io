a591f567309b30bc810e10e1077965009a7b574f
meeeetm <153938701+meeeetm@users.noreply.github.com>
2024-03-29 03:58:04 +0100

    Add files via upload

--- 500-Yeni/Dailymotin-Player/script.js
+++ 500-Yeni/Dailymotin-Player/script.js
@@ -51,6 +51,193 @@ dailymotion
       video: 'x8sf8zo',
     })
 
+ dailymotion
+    .createPlayer('my-dailymotion-player-11', {
+      video: 'x8sux2i',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-12', {
+      video: 'x8piphq',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-13', {
+      video: 'x8sqs2w',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-14', {
+      video: 'x8sh1ny',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-15', {
+      video: 'x8sh9a2',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-16', {
+      video: 'x8pfax4',
+    })
+ dailymotion
+    .createPlayer('my-dailymotion-player-17', {
+      video: 'x8scsx8',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-18', {
+      video: 'x8phcdh',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-19', {
+      video: 'x8sh9a4',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-20', {
+      video: 'x8sdq2q',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-21', {
+      video: 'x8sf8zm',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-22', {
+      video: 'x8phdxr',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-23', {
+      video: 'x8sux2k',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-24', {
+      video: 'x8pfbjx',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-25', {
+      video: 'x8swjsi',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-26', {
+      video: 'x8pni8c',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-27', {
+      video: 'x8phcki',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-28', {
+      video: 'x8sux2o',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-29', {
+      video: 'x8q3u45',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-30', {
+      video: 'x8sqs2u',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-31', {
+      video: 'x8q3tha',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-32', {
+      video: 'x8q3vcj',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-33', {
+      video: 'x8smt9m',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-34', {
+      video: 'x8sl9sc',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-35', {
+      video: 'x8nzaw3',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-36', {
+      video: 'x8sqs2y',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-37', {
+      video: 'x8sqs2s',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-38', {
+      video: 'x8pniqc',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-39', {
+      video: 'x8smt9m',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-40', {
+      video: 'x8niejd',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-40', {
+      video: 'x8npo4c',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-41', {
+      video: 'x5nunyk',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-42', {
+      video: 'x8863vn',
+    })
+
+ dailymotion
+    .createPlayer('my-dailymotion-player-43', {
+      video: 'x71t7j',
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
