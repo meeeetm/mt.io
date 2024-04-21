736ded92eb5b7c672fb8d653caa481201e0670d5
meeeetm <153938701+meeeetm@users.noreply.github.com>
2024-04-21 15:41:58 +0200

    Delete Indir1 directory

--- Indir1/modules/contrib/open_readspeaker/js/post_mode.js
+++
@@ -1,8 +0,0 @@
-(function (Drupal) {
-  'use strict';
-  Drupal.behaviors.open_readspeaker_post_mode = {
-    attach: function (context, settings) {
-      window.rsConf = { general: { usePost: true } };
-    }
-  };
-})(Drupal);
